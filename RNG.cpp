14fe7b936a46bb5f2e7c6ce1e65258fd5a75ac2c
tinyynoob <sadpotato1920@gmail.com>
2022-03-13 15:38:51 +0800

    Add RNG.cpp and generate test cases for 3.c
    2000 pairs of numbers for each case.
    Range:
    - small case: 0 ~ 65535
    - big case: 65536 ~ 4294967295

---
+++ RNG.cpp
@@ -0,0 +1,21 @@
+#include <random>
+#include <iostream>
+#include <fstream>
+
+/* refer to https://stackoverflow.com/questions/28115724/getting-big-random-numbers-in-c-c */
+
+int main()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uint64_t lowbound = 65536, upbound = 4294967295;
+    std::uniform_int_distribution<unsigned long long> dis(lowbound, upbound);
+
+    std::fstream f;
+    f.open("RNG.txt", std::ios::out | std::ios::trunc);
+    for (size_t i = 0; i < 2000; ++i)
+        f << dis(gen) << ' ' << dis(gen) << std::endl;
+
+    f.close();
+    return 0;
+}
